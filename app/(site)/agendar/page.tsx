ee2f54d871eb6a24cabfb8390d01d0373ba7f6f1
RF2025BYO <renansoaresfonseca@gmail.com>
2026-07-15 19:49:18 -0300

    Create page.tsx

---
+++ app/(site)/agendar/page.tsx
@@ -0,0 +1,11 @@
+const Agendar = () => {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Página Google Ads
+      </h1>
+    </section>
+  );
+};
+
+export default Agendar;
